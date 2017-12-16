1869d0f4b134d00fa0a38f92c6d310c80a751640
hjsiso <hjsiso@gmail.com>
2017-12-16 17:51:36 -0500

    Product Search beta

--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -10,13 +10,19 @@ class Products extends Component {
         this.state = {
             currentItem: '',
             items: [],
-            categories: [],
+            allItems: [],
+            categories: null,
+            categoriesArray: [],
             showEditOpts: false,
-            currentOrder: 'price'
+            currentOrder: 'price',
+            currentCategory: '',
+            currentSearch: '',
         }
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeCategory = this.handleChangeCategory.bind(this);
+        this.handleChangeSearch = this.handleChangeSearch.bind(this);
+
     }
 
     componentDidMount() {
@@ -28,7 +34,7 @@ class Products extends Component {
                 newState.push({
                     id: item,
                     name: items[item].name,
-                    thumbnail: items[item].thumbnail,
+                    thumbnail: items[item].thumbnail + "/" + items[item].name,
                     description: items[item].description,
                     price: items[item].price,
                     category: items[item].category
@@ -38,63 +44,14 @@ class Products extends Component {
             newState = _.orderBy(newState, this.state.currentOrder);
 
             this.setState({
-                items: newState
+                items: newState,
+                allItems: newState
             });
 
         });
 
         this.loadCategories();
-    }
 
-    editItem(item, e) {
-        e.preventDefault();
-        //console.log(item);
-        this.setState({
-            currentItem: item.id,
-            productName: item.productName,
-            showEditOpts: true
-        })
-    }
-
-    delItem(item, e) {
-        e.preventDefault();
-        //console.log(item);
-        let category = prompt("Por favor escriba la categoria a eliminar", "");
-
-        if (category == item.productName) {
-
-            if (confirm('Seguro que desea eliminar la categoria ' + category + ' ?')) {
-                const itemsRef = firebase.database().ref('categories/' + item.id).remove();
-                this.setState({
-                    currentItem: '',
-                    productName: '',
-                    showEditOpts: false
-                })
-            }
-        }
-
-
-    }
-
-    updateItem(e) {
-        e.preventDefault();
-        //console.log(this.state.currentItem);
-        const itemsRef = firebase.database().ref('categories/' + this.state.currentItem).update({
-            productName: this.state.productName
-        });
-        this.setState({
-            currentItem: '',
-            productName: '',
-            showEditOpts: false
-        })
-    }
-
-    hideEditOpts(e) {
-        this.setState({
-            showEditOpts: false,
-            productName: '',
-            currentItem: ''
-        })
     }
 
     handleChange(e) {
@@ -105,68 +62,101 @@ class Products extends Component {
 
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        const itemsRef = firebase.database().ref('categories');
-        const item = {
-            productName: this.state.productName,
-            visible: true
+    handleChangeCategory(e) {
+        if (e.target.value == "") {
+            this.setState({
+                items: _.orderBy(this.state.allItems, this.state.currentOrder)
+            });
+        } else {
+            let tmpItem = _.filter(this.state.allItems, (item) => {
+                return item.category == e.target.value
+            });
+
+            this.setState({
+                items: _.orderBy(tmpItem, this.state.currentOrder)
+            });
         }
-        itemsRef.push(item);
-        this.setState({
-            productName: '',
-            currentItem: ''
-        });
     }
 
-    loadCategories(){
+    handleChangeSearch(e) {
+        if (e.target.value == "") {
+            this.setState({
+                items: _.orderBy(this.state.allItems, this.state.currentOrder)
+            });
+        } else {
+            let updatedList = _.filter(this.state.allItems, (item) => {
+                return item.name.toLowerCase().search(
+                    e.target.value.toLowerCase()) !== -1;
+            });
+            this.setState({ items: updatedList });
+        }
+    }
+
+    loadCategories() {
         const itemsRef = firebase.database().ref('/');
         itemsRef.child("categories").on('value', (snapshot) => {
             let items = snapshot.val();
             let newState = [];
-           /* for (let item in items) {
+            for (let item in items) {
                 newState.push({
                     id: item,
                     categoryName: items[item].categoryName
                 });
-            }*/
-            newState = items;
+            }
+
             this.setState({
-                categories: newState
+                categories: snapshot.val(),
+                categoriesArray: newState
             });
 
-            console.log("CATEGORIAS " + newState);
         });
+
+
+    }
+
+    filterList(){
+
+    }
+
+    onDropdownSelected(e) {
+        console.log("THE VAL", e.target.value);
+        //here you will see the current selected value of the select input
     }
 
     render() {
         return (
             <div className="animated fadeIn">
-
-
-
                 <table className="table table-sm table-inverse mb-2">
                     <thead>
                         <tr>
                             <th colSpan="3">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col col-sm-2">
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col col-sm-2">
                                             CATEGORIA:
                                         </div>
-                                        <div class="col col-sm-2">
-                                            <select class="form-control form-control-sm">
-                                                <option selected>Todas</option>
+                                        <div className="col col-sm-2">
+                                            <select className="form-control form-control-sm" onChange={this.handleChangeCategory}>
+                                                <option value="">Todas</option>
+                                                {
+                                                    this.state.categoriesArray.map((item) => {
+                                                        return (
+                                                            <option key={item.id} value={item.id}>{item.categoryName}</option>
+                                                        )
+                                                    })}
                                             </select>
                                         </div>
-                                        <div class="col col-sm-2">
+                                        <div className="col col-sm-2">
                                             ORDENAR POR:
                                         </div>
-                                        <div class="col col-sm-2">
-                                            <select class="form-control form-control-sm" onChange={this.handleChange}>
+                                        <div className="col col-sm-2">
+                                            <select className="form-control form-control-sm" onChange={this.handleChange}>
                                                 <option value="price">Precio</option>
                                                 <option value="name">Nombre</option>
                                             </select>
+                                        </div>
+                                        <div className="col col-sm-4">
+                                            <input type="text" placeholder="Buscar..." className="form-control form-control-sm" onChange={this.handleChangeSearch} />
                                         </div>
                                     </div>
                                 </div>
@@ -177,25 +167,25 @@ class Products extends Component {
                     <tbody>
                         {
                             this.state.items.map((item) => {
-                            return (
-                                <tr key={item.id}>
-                                    <td width="100px">
-                                        <img src={item.thumbnail} class="rounded float-left" alt={item.name} />
-                                    </td>
-                                    <td>
-                                        <h4>{item.name}</h4>
-                                        <small>{item.description}</small>
-                                        <br/>
-                                        <span class="badge badge-pill badge-info">{_.get(this.state.categories, `${item.category}.categoryName`)}</span>
-                                    </td>
-                                    <td width="150px">
-                                        <span class="badge badge-pill badge-primary">
-                                            <NumberFormat value={item.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
-                                        </span>
-                                    </td>
-                                </tr>
-                            )
-                        })}
+                                return (
+                                    <tr key={item.id}>
+                                        <td width="100px">
+                                            <img src={item.thumbnail} className="rounded float-left" alt={item.name} />
+                                        </td>
+                                        <td>
+                                            <h4>{item.name}</h4>
+                                            <small>{item.description}</small>
+                                            <br />
+                                            <span className="badge badge-pill badge-info">{_.get(this.state.categories, `${item.category}.categoryName`)}</span>
+                                        </td>
+                                        <td width="150px">
+                                            <span className="badge badge-pill badge-primary">
+                                                <NumberFormat value={item.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                                            </span>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
                     </tbody>
                 </table>
 
@@ -206,5 +196,7 @@ class Products extends Component {
     }
 
 }
+
+
 
 export default Products;
